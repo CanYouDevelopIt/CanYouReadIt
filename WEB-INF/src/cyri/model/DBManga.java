ee9f88f9e8ca97802b64d321db9d9575da2790f6
jkn1092 <jkn1092@gmail.com>
2015-06-03 14:35:22 +0200

    bdd

--- WEB-INF/src/cyri/model/DBManga.java
+++ WEB-INF/src/cyri/model/DBManga.java
@@ -1,12 +1,27 @@
 package cyri.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class DBManga {
 
 	public ArrayList<Manga> listManga = new ArrayList<Manga>();
-
+	private static DBManga INSTANCE;
+	private String url = "jdbc:mysql://localhost:3306/cyri";
+	private String user = "cyri";
+	private String motDePasse = "cyri";
+	Connection connexion = null;
+	
 	private DBManga() {
+		
+//		try {
+//			connexion = DriverManager.getConnection(url, user, motDePasse);
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+		
 		listManga.add(new Manga("Shingeki No Kyojin"));
 		listManga.add(new Manga("One Piece"));
 		listManga.add(new Manga("Naruto"));
@@ -24,8 +39,6 @@ public class DBManga {
 		}
 		return null;
 	}
-
-	private static DBManga INSTANCE;
 
 	public static DBManga getInstance() {
 		if (INSTANCE == null)
